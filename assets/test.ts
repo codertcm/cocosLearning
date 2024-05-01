6ea8170eba6249ec04e4cea641d5185855080d07
codertcm <codertcm@gmail.com>
2024-05-01 12:16:49 +0800

    course 36

--- assets/test.ts
+++ assets/test.ts
@@ -21,7 +21,22 @@ export default class NewClass extends cc.Component {
     // onLoad () {}
 
     start () {
+        // http://www.base.com/1.png
+        let self = this;
+        // cc.loader.loadRes("test/1/fragment_6", cc.SpriteFrame, function(err, sp){
+        //     //...(1) 图片加载完成后执行
+        //     self.getComponent(cc.Sprite).spriteFrame = sp;
 
+        // });
+
+        // 图集加载
+        cc.loader.loadRes("test/1", cc.SpriteAtlas, function(err, altas: cc.SpriteAtlas){
+            self.getComponent(cc.Sprite).spriteFrame = altas.getSpriteFrame("fragment_6");
+        });
+
+        //...(2)
+
+        // (1)(2)谁先执行不确定
     }
 
     // update (dt) {}
