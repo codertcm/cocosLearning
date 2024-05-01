678f9973b9e7930e00a902b0cef6172ed7530a9b
codertcm <codertcm@gmail.com>
2024-05-01 16:17:51 +0800

    course 39

--- assets/test.ts
+++ assets/test.ts
@@ -22,6 +22,23 @@ export default class NewClass extends cc.Component {
 
     start () {
 
+        // 触摸事件
+        let self = this;
+        this.node.on(cc.Node.EventType.TOUCH_START, function(event){
+            console.debug("触摸:" + event.getLocation());
+            // self.node.emit("myevent");
+
+            // 冒泡传递
+            // self.node.dispatchEvent(new cc.Event.EventCustom("myevent", true));
+
+            
+
+        });
+
+        // 监听自定义事件
+        this.node.on("myevent", function(event){
+            console.debug("自定义事件");
+        });
     }
 
     // update (dt) {}
